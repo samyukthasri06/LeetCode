f141e98c046fa5fb230f8fef42dbe292f307cecf
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:18:27 +0530

    Create 91-DecodeWays

---
+++ 91-DecodeWays/91-DecodeWays.java
@@ -0,0 +1,27 @@
+// Last updated: 7/17/2026, 3:16:32 PM
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int current = 0;
+
+            if (s.charAt(i) != '0') {
+                current += prev1;
+            }
+
+            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                current += prev2;
+            }
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
