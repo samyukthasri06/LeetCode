05741c7451dc9da03179d7808bfb1b49794c1135
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:16:59 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,27 @@
+// Last updated: 7/17/2026, 3:15:04 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            count[s.charAt(i) - 'a']++;
+
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num : count) {
+
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
