058d7e4300e766a6c0b4a68f0230e0ecd225e172
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:17:37 +0530

    Create 183-CustomersWhoNeverOrder

---
+++ 183-CustomersWhoNeverOrder/183-CustomersWhoNeverOrder.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/17/2026, 3:15:41 PM
+# Write your MySQL query statement below
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+    ON c.id = o.customerId
+WHERE o.customerId IS NULL;
