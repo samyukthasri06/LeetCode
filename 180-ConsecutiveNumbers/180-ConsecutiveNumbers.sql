4babb2871bea9bf2a73703252ec46501c5d782b5
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:17:41 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/17/2026, 3:15:46 PM
+# Write your MySQL query statement below
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2
+    ON l1.id = l2.id - 1
+JOIN Logs l3
+    ON l1.id = l3.id - 2
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
