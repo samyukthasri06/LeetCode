5566696b11f329d724f4ec2ea9942336ef288a9e
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:19:58 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.java
@@ -0,0 +1,39 @@
+// Last updated: 7/17/2026, 3:18:03 PM
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prevGroup = dummy;
+
+        while (true) {
+
+            ListNode kth = prevGroup;
+
+            for (int i = 0; i < k && kth != null; i++) {
+                kth = kth.next;
+            }
+
+            if (kth == null) break;
+
+            ListNode groupNext = kth.next;
+
+            ListNode prev = groupNext;
+            ListNode curr = prevGroup.next;
+
+            while (curr != groupNext) {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            ListNode temp = prevGroup.next;
+            prevGroup.next = kth;
+            prevGroup = temp;
+        }
+
+        return dummy.next;
+    }
+}
