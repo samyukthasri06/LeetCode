d389af74fc2d1d05363fff27bbb7a3dfd215b7c1
samyukthasri06 <samyukthasri684@gmail.com>
2026-07-17 15:16:30 +0530

    Create 874-BackspaceStringCompare

---
+++ 874-BackspaceStringCompare/874-BackspaceStringCompare.java
@@ -0,0 +1,20 @@
+// Last updated: 7/17/2026, 3:14:35 PM
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    private String build(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (c != '#') {
+                sb.append(c);
+            } else if (sb.length() > 0) {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+
+        return sb.toString();
+    }
+}
